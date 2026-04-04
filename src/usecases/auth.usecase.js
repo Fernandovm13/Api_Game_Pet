c9958cb628aa375d4aed0290e93e73e693bbcc0d
Fernando Velez Mendez <233422@ids.upchiapas.edu.mx>
2026-04-04 10:52:08 -0600

    Nombre se usuario

--- src/usecases/auth.usecase.js
+++ src/usecases/auth.usecase.js
@@ -6,10 +6,16 @@ class AuthUseCase {
     let user = await userRepository.findByFirebaseUid(uid);
 
     if (!user) {
+      let defaultName = 'Atleta';
+      if (email) {
+        const parts = email.split('@');
+        defaultName = parts[0].charAt(0).toUpperCase() + parts[0].slice(1);
+      }
+
       await userRepository.create({
         firebaseUid: uid,
         email: email || null,
-        displayName: name || 'Atleta'
+        displayName: name || defaultName 
       });
       user = await userRepository.findByFirebaseUid(uid);
     }
